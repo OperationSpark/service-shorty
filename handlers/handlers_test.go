058303a96d69be09ffb43a96e9880a3691bf2d45
Peter Barnum <peterbarnum@gmail.com>
2023-08-26 11:10:21 -0600

    git short code handler test

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -10,17 +10,19 @@ func TestParseLinkCode(t *testing.T) {
 	t.Run("grabs the Link code from the URL", func(t *testing.T) {
 		tests := []struct {
 			url  string
-			want string
+			want *ShortCodeData
 		}{
-			{"/api/urls", ""},
-			{"/api/urls/", ""},
-			{"/api/urls/abc123", "abc123"},
-			{"/api/urls/abc123/", "abc123"},
+			{"/api/urls", &ShortCodeData{code: "", tag: ""}},
+			{"/api/urls/", &ShortCodeData{code: "", tag: ""}},
+			{"/api/urls/abc123", &ShortCodeData{code: "abc123", tag: ""}},
+			{"/api/urls/abc123/", &ShortCodeData{code: "abc123", tag: ""}},
+			{"/api/urls/abc123/t-def456", &ShortCodeData{code: "abc123", tag: "t-def456"}},
 		}
 
 		for _, c := range tests {
 			got := parseLinkCode(c.url)
-			testutil.AssertEqual(t, got, c.want)
+			testutil.AssertEqual(t, got.code, c.want.code)
+			testutil.AssertEqual(t, got.tag, c.want.tag)
 		}
 	})
 }
